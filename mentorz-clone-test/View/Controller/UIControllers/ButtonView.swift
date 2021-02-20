8be5ddb22e15c8da74d4d69da293ac5e5eb7cd52
coderode <sk9958814616@gmail.com>
2021-02-20 23:41:49 +0530

    splash page

---
+++ mentorz-clone-test/View/Controller/UIControllers/ButtonView.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+class ButtonView: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+        backgroundColor = #colorLiteral(red: 0.968627451, green: 0.1803921569, blue: 0.2196078431, alpha: 1)
+        setTitleColor(UIColor.white, for: UIControl.State.normal)
+        titleLabel!.font = UIFont.boldSystemFont(ofSize: CGFloat(23))
+        tintColor = .black
+        contentHorizontalAlignment = .center
+        contentVerticalAlignment = .center
+        contentEdgeInsets = UIEdgeInsets(top: 18,left: 0,bottom: 18,right: 0)
+    }
+}
