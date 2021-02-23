6ffe5f668b310b0b3fa0596db430d9ea4a79f22a
coderode <sk9958814616@gmail.com>
2021-02-23 21:47:28 +0530

    completed forgot password and signup and login page

--- mentorz-clone-test/View/ViewControllers/LoginPageVC.swift
+++ mentorz-clone-test/View/ViewControllers/LoginPageVC.swift
@@ -16,27 +16,46 @@ class LoginPageVC: UIViewController {
     @IBOutlet weak var orLabel: UILabel!
     @IBOutlet weak var switchToSignupPageButton: BottomSwitchButton!
     @IBOutlet weak var bottomLinkButton: LoginSignupBottomLinkButton!
+    let secureButton = UIButton(type: .custom)
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImage.image = UIImage(named: "bg")
         backgroundImage.alpha = 0.1
         LoginSignupTextField.textFieldDesign(phoneNumber, "Phone number")
-        LoginSignupTextField.textFieldDesign(password, "Password")
+        LoginSignupTextField.textFieldDesign(password,"Password")
         LoginSignupTextField.countryCodePickerDesign(countryCodePicker)
         orLeftView.addTopBorderWithColor(color: .gray, width: 1)
         orRightView.addTopBorderWithColor(color: .gray, width: 1)
+        
+        //password secure or nonsecure
+        self.secureButton.setImage(UIImage(named: "eye_2"), for: .normal)
+        self.secureButton.tintColor = .darkGray
+        self.secureButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        self.secureButton.frame = CGRect(x: 0, y: 0, width: self.password.frame.size.width/8, height: self.password.frame.size.height)
+        self.secureButton.addTarget(self, action: #selector(self.btnPasswordVisibilityTapped), for: .touchUpInside)
+        self.password.trailingView = self.secureButton
+        self.password.trailingViewMode = .always
+        self.password.isSecureTextEntry = true
+        
+        //actions
         forgotPasswordButton.addTarget(self, action: #selector(didForgotPasswordButtonTapped), for: .touchUpInside)
         switchToSignupPageButton.addTarget(self, action: #selector(didSwitchToSignupButtonTapped), for: .touchUpInside)
+    }
+    @objc func btnPasswordVisibilityTapped(_ sender : Any){
+        (sender as! UIButton).isSelected = !(sender as! UIButton).isSelected
+        if (sender as! UIButton).isSelected {
+            password.isSecureTextEntry = false
+            secureButton.setImage(UIImage(named: "eye"), for: .normal)
+        }else{
+            password.isSecureTextEntry = true
+            secureButton.setImage(UIImage(named: "eye_2"), for: .normal)
+        }
     }
     @objc func didForgotPasswordButtonTapped(){
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ForgotPasswordVC") as! ForgotPasswordVC
         nextViewController.modalPresentationStyle = .fullScreen
         self.present(nextViewController, animated: false, completion: nil)
-//        weak var pvc = self.presentingViewController
-//        self.dismiss(animated: false, completion: {
-//            pvc?.present(nextViewController, animated: false, completion: nil)
-//        })
     }
     @objc func didSwitchToSignupButtonTapped(){
         self.dismiss(animated: false, completion: nil)
--- mentorz-clone-test/View/ViewControllers/SignupPageVC.swift
+++ mentorz-clone-test/View/ViewControllers/SignupPageVC.swift
@@ -13,6 +13,7 @@ class SignupPageVC: UIViewController {
     @IBOutlet weak var password: MDCFilledTextField!
     @IBOutlet weak var signupButton: ButtonView!
     @IBOutlet weak var loginPageSwitch: BottomSwitchButton!
+    let secureButton = UIButton(type: .custom)
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImage.image = UIImage(named: "bg")
@@ -23,9 +24,33 @@ class SignupPageVC: UIViewController {
         LoginSignupTextField.textFieldDesign(phoneNumber, "Phone number")
         LoginSignupTextField.textFieldDesign(emailAddress, "Enter your email address")
         LoginSignupTextField.textFieldDesign(password, "Password")
+        
+        //secure or nonsecure password text field
+        self.secureButton.setImage(UIImage(named: "eye_2"), for: .normal)
+        self.secureButton.tintColor = .darkGray
+        self.secureButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        self.secureButton.frame = CGRect(x: 0, y: 0, width: self.password.frame.size.width/8, height: self.password.frame.size.height)
+        self.secureButton.addTarget(self, action: #selector(self.btnPasswordVisibilityTapped), for: .touchUpInside)
+        self.password.trailingView = self.secureButton
+        self.password.trailingViewMode = .always
+        self.password.isSecureTextEntry = true
+        
+        
+        //actions
         loginPageSwitch.addTarget(self, action: #selector(loginPageSwitchButtonTapped), for: .touchUpInside)
         backPageButton.addTarget(self, action: #selector(backPageButtonTapped), for: .touchUpInside)
         backPageButton.titleLabel?.text = nil
+    }
+    
+    @objc func btnPasswordVisibilityTapped(_ sender : Any){
+        (sender as! UIButton).isSelected = !(sender as! UIButton).isSelected
+        if (sender as! UIButton).isSelected {
+            password.isSecureTextEntry = false
+            secureButton.setImage(UIImage(named: "eye"), for: .normal)
+        }else{
+            password.isSecureTextEntry = true
+            secureButton.setImage(UIImage(named: "eye_2"), for: .normal)
+        }
     }
     
     @objc func loginPageSwitchButtonTapped(){
